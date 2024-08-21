e611e8e7524e94bbec4c223083a2fcad84df6fc5
hoainam1111 <66334691+hoainam1111@users.noreply.github.com>
2024-08-22 04:00:13 +0700

    update feature show edit update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
-    def new
-      @user = User.new
+    def index
+        @users = User.all
     end
-  
+    def show
+        @user = User.find(params[:id])
+    end
+    def new
+        @user = User.new
+    end
+    def edit
+        @user = User.find(params[:id])
+    end
     def create
         @user = User.new(user_params)
       
@@ -11,8 +19,16 @@ class UsersController < ApplicationController
         else
           render :new, status: :unprocessable_entity
         end
-      end
-  
+    end
+    def update
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+          redirect_to user_path, notice: "User was successfully updated."
+        else
+          render :edit
+        end
+    end
     private
   
     def user_params
